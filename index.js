db31405845d7b9e5b6c1791278d748955684e6c9
tjfstr <tjfosterish@gmail.com>
2020-11-25 11:05:39 -0500

    add toggle todos

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const express = require('express'),
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello from the root route');
+    res.sendFile('index.html')
 });
 
 app.use('/api/todos', todoRoutes);
